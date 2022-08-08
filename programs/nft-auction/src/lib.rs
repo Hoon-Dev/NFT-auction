062addbbfb827ed52d3e5b6085bed39d6b58c0c8
Kim Jae Hoon <hoon.development@gmail.com>
2022-08-09 01:38:52 +0900

    chore: 어카운트 제약조건 추가

--- programs/nft-auction/src/lib.rs
+++ programs/nft-auction/src/lib.rs
@@ -32,6 +32,11 @@ pub mod nft_auction {
 
     pub fn initialize_creator(ctx: Context<InitializeCreator>) -> Result<()> {
         ctx.accounts.mint_collection_token()?;
+        ctx.accounts.create_master_edition()?;
+        Ok(())
+    }
+
+    pub fn create_nft(ctx: Context<CreateNft>) -> Result<()> {
         Ok(())
     }
 }
@@ -45,25 +50,51 @@ pub struct InitializeCreator<'info> {
         init,
         payer = creator,
         mint::decimals = 0,
-        mint::authority = creator
+        mint::authority = creator,
+        seeds = [
+            creator.key().as_ref(),
+            b"collection".as_ref()
+        ],
+        bump
     )]
     pub nft_collection_mint: Account<'info, Mint>,
 
     #[account(
+        mut,
         associated_token::mint = nft_collection_mint,
         associated_token::authority = creator
     )]
     pub nft_collection_token_account: Account<'info, TokenAccount>,
 
-    /// CHECK: NEED VERIFY
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            nft_collection_mint.key().as_ref()
+        ],
+        bump = 254,
+        seeds::program = mpl_token_metadata::ID
+    )]
     pub nft_collection_metadata: AccountInfo<'info>,
 
-    /// CHECK: NEED VERIFY
+    /// CHECK: NFT 콜렉션 마스터 에디션 PDA
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            nft_collection_mint.key().as_ref(),
+            b"edition".as_ref()
+        ],
+        bump = 254,
+        seeds::program = mpl_token_metadata::ID
+    )]
     pub nft_collection_master_edition: AccountInfo<'info>,
 
-    /// CHECK: NEED VERIFY
+    /// CHECK: 메타데이터 프로그램
+    #[account(address = mpl_token_metadata::ID)]
     pub metadata_program: AccountInfo<'info>,
-
     #[account(address = rent::ID)]
     pub rent: Sysvar<'info, Rent>,
     #[account(address = token::ID)]
@@ -86,7 +117,15 @@ impl<'info> InitializeCreator<'info> {
                 }
             ),
             1 as u64
-        )?;
+        )
+    }
+    pub fn create_master_edition(&self) -> Result<()> {
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct CreateNft<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
 }
