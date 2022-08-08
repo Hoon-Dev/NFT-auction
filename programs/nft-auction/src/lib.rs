6415944c133f31a4b895082e110acbc393486d8b
Kim Jae Hoon <hoon.development@gmail.com>
2022-08-08 16:28:58 +0900

    chore: todo 리스트 작성

--- programs/nft-auction/src/lib.rs
+++ programs/nft-auction/src/lib.rs
@@ -2,6 +2,13 @@ use anchor_lang::prelude::*;
 
 declare_id!("HBUhjeux3yF45gErBG2NVNnuoAG9Pck3SVA34ruAcUig");
 
+/*
+    TODO:
+    1. NFT collection 민트, 토큰 어카운트
+    2. NFT 작품 민트, 토큰 어카운트
+    3. NFT 작품 Verify
+*/
+
 #[program]
 pub mod nft_auction {
     use super::*;
