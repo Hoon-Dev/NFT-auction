28811ccbff7a125d597d5ab6671e8abf43c73872
Kim Jae Hoon <hoon.development@gmail.com>
2022-08-08 16:24:59 +0900

    chore: 테스트 코드 셋팅

--- tests/nft-auction.ts
+++ tests/nft-auction.ts
@@ -2,16 +2,31 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { NftAuction } from "../target/types/nft_auction";
 
-describe("vue-anchor-boilerplate", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+import {
+  ConfirmOptions
+} from "@solana/web3.js";
+
+describe("NFT Auction House", () => {
+  const connection = new anchor.web3.Connection(
+    "http://127.0.0.1:8899",
+    "confirmed"
+  );
+
+  const wallet = anchor.Wallet.local();
+
+  const confirmOptions: ConfirmOptions = {
+    commitment: "confirmed",
+    preflightCommitment: "confirmed"
+  };
+  
+  anchor.setProvider(
+    new anchor.AnchorProvider(
+      connection,
+      wallet,
+      confirmOptions
+    )
+  );
 
   const program = anchor.workspace.NftAuction as Program<NftAuction>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
 });
 
